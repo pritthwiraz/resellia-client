d5f3adb71cf03acfe6362f9a8bb70a0bb6752d53
pritthwiraz <pritthwiraz@gmail.com>
2022-11-27 18:35:05 +0600

    Login page fixed

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import loginimg from '../../Resources/Images/images/login.png'
 
 const Login = () => {
-    const { register, handleSubmit } = useForm();
-    const [data, setData] = useState('');
+    const { register, formState: { errors }, handleSubmit } = useForm();
+  
 
     const handleLogin = data => {
         console.log(data);
@@ -24,13 +24,15 @@ const Login = () => {
                                 <label className="label">
                                     <span className="label-text">Email</span>    
                                 </label>
-                                <input type="text" {...register("email")} className="input input-bordered w-full max-w-xs"/>    
+                                <input type="text" {...register("email", {required: "Email Address is required"})} className="input input-bordered w-full max-w-xs"/>
+                                {errors.email && <p className='text-red-700'>{errors.email?.message}</p>}
                             </div>
                             <div className="form-control w-full max-w-xs">
                                 <label className="label">
                                     <span className="label-text">Password</span>    
                                 </label>
-                                <input type="text" {...register("password")} className="input input-bordered w-full max-w-xs"/>    
+                                <input type="text" {...register("password",{required: "Password is required", minLength:{ value: 6, message:"passwword must be 6 charaters or longer"}})} className="input input-bordered w-full max-w-xs"/>
+                                {errors.password && <p className='text-red-700'>{errors.password?.message}</p>}    
                             </div>
                 
                             <input className='btn w-full mt-6' type="submit" />
